914a61912ba1e6f4a072afecc970e64621fd33ba
awardak <amanwardak@gmail.com>
2014-12-15 15:27:08 -0800

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by User on 12/15/14.
+//  Copyright (c) 2014 aw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tipcalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipcalculator
+//
+//  Created by User on 12/15/14.
+//  Copyright (c) 2014 aw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
